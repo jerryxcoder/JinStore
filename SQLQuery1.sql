c236decc19d99a025cb5c826daa6c86e468a76ef
JerryDuh <jingangxiao@gmail.com>
2016-12-29 21:11:18 -0600

    name change

---
+++ SQLQuery1.sql
@@ -0,0 +1,26 @@
+﻿create database Member
+use Member
+
+CREATE TABLE Customer(
+	ID int IDENTITY(1, 1) NOT NULL,
+	FirstName nvarchar(100) NULL,
+	LastName nvarchar(100) NULL,
+	DateCreated DATETIME NOT NULL DEFAULT(GetUtcDate()),
+	DateLastModified DATETIME NOT NULL DEFAULT(GetUtcDate())
+	PRIMARY KEY(ID)
+)
+
+INSERT INTO Customer(FirstName, LastName) VALUES
+('Jin', 'Xiao'),
+('Tessa', 'Konkol'),
+('Serkan', 'Nizam'),
+('Jerry', 'Bony'),
+('Jimmy', 'Ellis')
+
+CREATE TABLE Courses(
+	Name nvarchar(100) NOT NULL,
+	[Description] ntext NULL,
+	DateCreated DATETIME NOT NULL DEFAULT(GetUtcDate()),
+	DateLastModified DATETIME NOT NULL DEFAULT(GetUtcDate())
+	PRIMARY KEY(Name)
+)
