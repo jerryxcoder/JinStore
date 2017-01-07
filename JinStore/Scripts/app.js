a7401dabda236c1ec63e71a801f1124da8a6c04c
JerryDuh <jingangxiao@gmail.com>
2017-01-07 17:49:49 -0600

    Added destination in angularjs

--- JinStore/Scripts/app.js
+++ JinStore/Scripts/app.js
@@ -1,15 +1,19 @@
 ﻿angular.module("JinStore", [])
 
+
+
 angular.module("JinStore").controller("TripsController", ["$http", "$scope", function ($http, $scope) {
 
     $http.get('/trips.json').success(function (data) {
-            $scope.trips = data;
-    });
-    //* activate code below to alow api access
+        $scope.trips = data;
+      });
+
+     //* activate code below to alow api access
     //$http.get('/api/trips').success(function (data) {
     //    $scope.trips = data;
     //});
 
+ 
 }]);
 
 
